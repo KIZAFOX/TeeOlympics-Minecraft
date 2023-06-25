6ac547663153fb073e800281831e386caa2c0271
KIZAFOX <victor.pernak@hotmail.fr>
2023-06-26 01:05:40 +0200

    :cyclone: chat format updated

--- src/main/java/fr/kizafox/teeolympics/managers/listeners/PlayerListeners.kt
+++ src/main/java/fr/kizafox/teeolympics/managers/listeners/PlayerListeners.kt
@@ -1,7 +1,10 @@
 package fr.kizafox.teeolympics.managers.listeners
 
 import fr.kizafox.teeolympics.core.TeeOlympicsCore
-import net.kyori.adventure.text.Component
+import io.papermc.paper.event.player.AsyncChatEvent
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -10,26 +13,27 @@ import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import kotlin.math.roundToInt
 
+
 class PlayerListeners(private var instance: TeeOlympicsCore?) : Listener {
 
     @EventHandler (priority = EventPriority.MONITOR)
     fun onLogin(event: PlayerJoinEvent){
         val player = event.player
 
-        event.joinMessage(Component.text("§7[§a+§7] §f${player.name}"))
+        event.joinMessage(text("§7[§a+§7] §f${player.name}"))
 
         for(i in 0..2){
-            player.sendMessage(Component.text(""))
+            player.sendMessage(text(""))
         }
-        player.sendMessage(Component.text("          Bienvenue sur §6§lTeeOlympics"))
-        player.sendMessage(Component.text(""))
-        player.sendMessage(Component.text("§8§l»§r N'oublie pas la commande §5§l/map§r pour voir la map en direct!"))
-        player.sendMessage(Component.text(""))
+        player.sendMessage(text("          Bienvenue sur §6§lTeeOlympics"))
+        player.sendMessage(text(""))
+        player.sendMessage(text("§8§l»§r N'oublie pas la commande §5§l/map§r pour voir la map en direct!"))
+        player.sendMessage(text(""))
     }
 
     @EventHandler (priority = EventPriority.MONITOR)
     fun onLogout(event: PlayerQuitEvent){
-        event.quitMessage(Component.text("§7[§c-§7] §f${event.player.name}"))
+        event.quitMessage(text("§7[§c-§7] §f${event.player.name}"))
     }
 
     @EventHandler (priority = EventPriority.MONITOR)
@@ -41,6 +45,13 @@ class PlayerListeners(private var instance: TeeOlympicsCore?) : Listener {
         val y = (location.y * 100).roundToInt() / 100.0
         val z = (location.z * 100).roundToInt() / 100.0
 
-        event.deathMessage(Component.text("§eCoordonnées de mort de §6${player.name}§e. §ex§7: §6$x §ey§7: §6$y §ez§7: §6$z §emonde§7: §6${location.world.name.uppercase()}"))
+        event.deathMessage(text("§eCoordonnées de mort de §6${player.name}§e. §ex§7: §6$x §ey§7: §6$y §ez§7: §6$z §emonde§7: §6${location.world.name.uppercase()}"))
+    }
+
+    @EventHandler (priority = EventPriority.MONITOR)
+    fun onChat(event: AsyncChatEvent){
+        event.isCancelled = true
+
+        Bukkit.getOnlinePlayers().forEach { players -> players.sendMessage(text("${event.player.name} : ${PlainTextComponentSerializer.plainText().serialize(event.message())}"))}
     }
 }

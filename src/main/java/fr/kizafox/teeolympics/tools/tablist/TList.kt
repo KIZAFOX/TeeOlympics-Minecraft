9357d745a384b4b98ed002b5e92945b4dfd95f2f
KIZAFOX <victor.pernak@hotmail.fr>
2023-06-26 04:30:30 +0200

    :cyclone: tablist

--- src/main/java/fr/kizafox/teeolympics/tools/tablist/TList.kt
+++ src/main/java/fr/kizafox/teeolympics/tools/tablist/TList.kt
@@ -1,10 +1,10 @@
 package fr.kizafox.teeolympics.tools.tablist
 
 import fr.kizafox.teeolympics.core.TeeOlympicsCore
+import fr.kizafox.teeolympics.tools.tps.TLag
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
-import org.bukkit.Bukkit
-import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitRunnable
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -12,14 +12,14 @@ import java.time.format.DateTimeFormatter
 
 class TList {
     companion object {
-        fun load(instance: TeeOlympicsCore?, player: Audience?) {
+        fun load(instance: TeeOlympicsCore?, player: Player?, audience: Audience?) {
             object : BukkitRunnable() {
                 override fun run() {
                     try {
                         val header: Component = Component.text("§6§lTeeOlympics")
-                        val footer: Component = Component.text("§8§k-§r §7Heure: §a${LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))} §8§k-")
+                        val footer: Component = Component.text("§7Ping: §e${player!!.ping} §8- §7Heure: §a${LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))} §8- §7TPS: §e${TLag.getLag()}")
 
-                        player!!.sendPlayerListHeaderAndFooter(header, footer)
+                        audience!!.sendPlayerListHeaderAndFooter(header, footer)
                     } catch (e: NoSuchFieldException) {
                         e.printStackTrace()
                     } catch (e: IllegalAccessException) {
